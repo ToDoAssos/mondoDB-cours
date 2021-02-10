64016b694b3df1089dfb78875b9287b8564091be
WiseM [PC Perso] <samuel.litzler39@gmail.com>
2021-02-10 14:32:31 +0100

    auth

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 const db = require("./app/models");
 db.mongoose.connect(db.url, {useNewUrlParser: true, useUnifiedTopology: true})
-// db.mongoose.connect(db.url, {useNewUrlParser: true, useUnifiedTopology: true, user: 'root', pass : 'root'})
+// db.mongoose.connect(db.url, {useNewUrlParser: true, useUnifiedTopology: true, user: 'root', pass : 'root', auth : {authdb : "admin"} } )
   .then(() => { console.log("Connected to the database!");})
   .catch(err => { console.log("Cannot connect to the database!", err); process.exit();});
 
